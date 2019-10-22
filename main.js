64d63905dad372aaf7386eaa8471bb158c2c8728
Muneera Bin Hotan <muneerabinhotan@gmail.com>
2019-10-22 22:06:36 +0300

    completed

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-// Remember your promises
-// Go back to the lesson if you feel lost
-// Read the docs well, they usually have pretty well documented examples
-// Consider making your fetch work with a button
 
-const apiUrl = 'https://rickandmortyapi.com/api/character/1,2,3,4,5';
+const rickAndMortyUrl = 'https://rickandmortyapi.com/api/character/1,2,3,4,5';
 const container = document.querySelector('.container');
 container.style.display='inline-block';
 const button = document.querySelector('#random');
 
-
-
 const getCharacters = () => {
     axios({
-        url: apiUrl,
+        url: rickAndMortyUrl,
         method: 'get' 
     })
 
         .then(result => {
             result.data.forEach( char => {
                 const img = document.createElement('img');
+                img.style.display='block';
+                img.style.margin='auto';
+
+
                 const name = document.createElement('p');
                 name.style.textAlign='center';
 
@@ -37,3 +35,40 @@ const getCharacters = () => {
         })
     }
 button.addEventListener('click', getCharacters);
+
+/////////////////////////////////////////////////
+
+
+
+const studioGhibliUrl = 'https://ghibliapi.herokuapp.com/films';
+
+
+const films = () => {
+    axios({
+        url: studioGhibliUrl,
+        method: 'get' 
+    })
+
+        .then(result => {
+            result.data.forEach( film => {
+                const filmTitle = document.createElement('h2');
+                filmTitle.style.textAlign='center';
+                
+                const filmDescription = document.createElement('p');
+                filmDescription.style.textAlign='center';
+
+                
+                filmTitle.innerText= film.title;
+                container.appendChild(filmTitle);
+
+                filmDescription.innerText= film.description;
+                container.appendChild(filmDescription);
+
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+button.addEventListener('click', films);
+
