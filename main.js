c1d0bac716709402afcaedf34a8b39a97d1f3f30
obal3588 <obal3588@colorado.edu>
2019-10-24 13:55:41 +0300

    done

--- main.js
+++ main.js
@@ -2,3 +2,33 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+
+  for (let i=0 ; i <5;i++){
+
+    const img = document.createElement('img');
+    const apiUrl = `https://rickandmortyapi.com/api/character/${i}`;
+
+    axios(
+        {
+            method: 'GET',
+            url: apiUrl
+           
+        }
+    )
+        .then(
+            ressponds => {
+                const imgUrl = ressponds.data.image;
+                img.src = imgUrl;
+                document.body.append(img)
+               
+            }
+
+        )
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+        
+
