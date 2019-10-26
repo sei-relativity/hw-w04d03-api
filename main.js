28e09793aedb9882cb298f455014f81b6aea3840
sumayahtechcampus <sumayah.ali.saleh@gmail.com>
2019-10-26 14:13:23 +0300

    solve HW

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-// Remember your promises
-// Go back to the lesson if you feel lost
-// Read the docs well, they usually have pretty well documented examples
-// Consider making your fetch work with a button
+const button = document.querySelector("button");
+const apiUrl = 'https://rickandmortyapi.com/api/character/';
+
+const getRickAndMorty = () => {
+
+for (let i = 1; i <= 5; i++) {
+
+const img = document.createElement("img");
+const charName = document.createElement("h2");
+
+axios({
+url: `${apiUrl}${i}`,
+method: "get"
+})
+.then(res => {
+img.src = res.data.image;
+charName.innerText = res.data.name;
+document.body.appendChild(document.createElement("br"));
+document.body.appendChild(charName);
+document.body.appendChild(img);
+})
+.catch(err => {
+    console.log(err);
+    });
+    }
+    };
+      button.addEventListener("click",getRickAndMorty )
