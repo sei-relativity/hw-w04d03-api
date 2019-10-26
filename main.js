58ab544cb6c3c2e4b500c218ab48321d2f0ffa2e
saera1988 <saera1988@hotmail.com>
2019-10-26 20:21:49 +0300

    final version

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
+'use strict';
 
-const rickImg = document.createElement('img'),
-mortyImg = document.createElement('img'),
-summerImg = document.createElement('img'),
-bethImg = document.createElement('img'),
-jerryImg = document.createElement('img'),
-apiUrl="https://rickandmortyapi.com/api/character/",
-button = document.querySelector("#random") ;
+// Assign variables 
 
-const getImg = () =>{
-axios ({
-    url : apiUrl,
-    method: 'get'
-})
-.then (res => {
+const button = document.querySelector('button');
+const apiUrl = 'https://rickandmortyapi.com/api/character/[1,2,3,4,5]';
 
-    console.log(res)
-            img.src=res.
-            document.body.appendChild(img)
+// Click event call back function
+const getImg = () => {
+   axios({
+       url: apiUrl,
+       method: 'GET'
+   })
+   .then( res => {
+       console.log(res)
+       // etration throught img and characters 
+       res.data.forEach(charecter => {
+           const name = document.createElement('p')
+           // type the character name 
+           name.innerText = charecter.name;
+           document.body.appendChild(name)
+       const img = document.createElement('img');
+       const imgUrl = charecter.image;
+       img.src = imgUrl;
+       document.body.appendChild(img);
+       });
+   })
+   .catch((error)=>{
+       // fails massege
+       console.log(error)
+})}
 
-
-})
-.catch (err => {
-    console.log(err)
-})
-
-
-
-}
+// Add event listener to the button
 button.addEventListener('click', getImg);
