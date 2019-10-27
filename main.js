015b6292660a362586614ba323cccf0061df89a0
Abdulrhman-J <a.jammaza@gmail.com>
2019-10-27 18:52:39 +0300

    add the function of axios to bring the pics

--- main.js
+++ main.js
@@ -2,3 +2,26 @@ const picUrl = 'https://rickandmortyapi.com/api/character/1,2,3,4,5';
 const container = document.querySelector('.container');
 const btn = document.querySelector('#random');
 
+
+const showCharacters = () => {
+    axios.get(picUrl)
+    .then(res => {
+        res.data.forEach(item => {
+            const img = document.createElement('img');
+            
+            let image = item.image;
+            img.src = image;
+            container.appendChild(img);
+        });
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+
+
+
+
+
+btn.addEventListener('click', showCharacters)
