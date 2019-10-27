da4949b899d317113638bd2e177a251f1e8b95f7
Lamafahad <lamafa97@gmail.com>
2019-10-27 08:52:04 +0300

    api

--- main.js
+++ main.js
@@ -2,3 +2,34 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+const url = 'https://rickandmortyapi.com/api/character/1,2,3,4,5';
+const container = document.querySelector('.container');
+const button = document.querySelector('#random');
+
+const displayImages = () => {
+    axios({
+        url: url,
+        method: 'get' 
+    })
+
+    .then(result => {
+    result.data.forEach( char => {
+    const img = document.createElement('img');
+
+    const name = document.createElement('p');
+    name.innerText= char.name;
+    container.appendChild(name);
+
+     let imgUrl= char.image;
+     img.src= imgUrl;
+     container.appendChild(img);
+     })
+        })
+
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+button.addEventListener('click', displayImages);
