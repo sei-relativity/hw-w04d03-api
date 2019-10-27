7025a38c0f6b39b37de4f9fb44ec6bde23397146
Abdulrhman-J <a.jammaza@gmail.com>
2019-10-27 18:43:11 +0300

    declare the varibles

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Remember your promises
-// Go back to the lesson if you feel lost
-// Read the docs well, they usually have pretty well documented examples
-// Consider making your fetch work with a button
+const picUrl = 'https://rickandmortyapi.com/api/character/1,2,3,4,5';
+const container = document.querySelector('.container');
+const btn = document.querySelector('#random');
+
